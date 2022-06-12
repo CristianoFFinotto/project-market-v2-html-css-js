3876375d7eabc89d33c4f808a696105b469bbb66
Davide Murroni <davide.murroni@edu.itspiemonte.it>
2022-06-12 04:26:41 +0200

    styled setting panel / implemented test.js

--- scripts/test.js
+++ scripts/test.js
@@ -3,11 +3,18 @@
 
 let openCloseMenu = () => {
   let mainContainer = document.getElementById("main-container");
+  let button = document.getElementById("setting-btn");
   if (mainContainer.className == "container") {
-    mainContainer.className += "-open"
+    mainContainer.className += "-open";  
   } else {
-    mainContainer.className = "container";
+    mainContainer.className = "container";  
   }
+  if (button.className == "setting-btn") {
+    button.className += "-open";
+  } else {
+    button.className = "setting-btn";
+  }
+
 };
 
 let settingButton = document.getElementById("setting-btn").addEventListener("click", openCloseMenu);
