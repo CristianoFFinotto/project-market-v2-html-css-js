65e0f4821b411d822414c8e264d7a9a3627a1967
Finotto CF <manganaruto715@gmail.com>
2022-06-14 18:24:24 +0200

    validate inputs form

--- scripts/config.mjs
+++ scripts/config.mjs
@@ -13,32 +13,12 @@
  * @property {number} shelfLife - N - number of weeks an item can be on the shelf before it is considered old
  * @property {number} newItemsPerWeek - M - numbr of new items added each week
  * @property {number} weeksRuntime - X - how many weeks the program will run for
- * @property {number} intervalSeconds - R - how many seconds a simulated weeks lasts for
- * @property {number} zeroPaddedDigits - the final length to reach after a number is 0 padded
- * @property {string} paddingCharacter - the character used to pad a string
- * @property {number} paddedNameChars - the final length to reach after the name of an item is padded
- * @property {number} paddedStateChars - the final lenght to reach after the state of an item is padded
- * @property {number} minIntervalSec - the lowest number of seconds an interval can assume
- * @property {number} maxIntervalSec - the highest number of seconds (excluded) an interval can assume
- * @property {string} newColor - the color used to style the output if the state of an item is "New"
- * @property {string} validColor - the color used to style the output if the state of an item is "Valid"
- * @property {string} oldColor - the color used to style the output if the state of an item is "Old"
- * @property {string} expiredColor - the color used to style the output if the state of an item is "Expired"
- * @property {string} fallbackColor - the color used to style the output if the item has no state
- * @property {string} locale - the locale used for the date: https://www.venea.net/web/culture_code
- * @property {string} dayFormat - used to format the day ("2-digit", "numeric")
- * @property {string} monthFormat - used to format the month ("long" "narrow" "numeric" "short")
- * @property {string} yearFormat - used to format the year ("2-digit", "numeric")
  */
+
 export let config = {
 	daysInWeek: 6,
 	startingOffset: 3,
 	shelfLife: 2,
 	newItemsPerWeek: 5,
 	weeksRuntime: 4,
-	intervalSeconds: 1,
-	zeroPaddedDigits: 3,
-	paddingCharacter: "*",
-	paddedNameChars: 17,
-	paddedStateChars: 11,
 };
--- scripts/main.mjs
+++ scripts/main.mjs
@@ -10,7 +10,47 @@
  import { config as cnf } from "./config.mjs"; //configuration object
  import * as fn from "./functions.mjs"; //main functions used to run the program
  import { itemNames } from "./itemsNames.mjs"; //array with a list of all possible item names
- 
+ import * as validator from "./validator.mjs";
+
+/********************** Taking DOM node **********************/
+
+const inputStartDate = document.querySelector('#start-date');
+const inputWeeks = document.querySelector('#weeks');
+const inputWeeklyProducts = document.querySelector('#weekly-products');
+const inputDaysInaWeek = document.querySelector('#days-in-week');
+const inputCheckTreshold = document.querySelector('#check-threshold');
+const inputSave = document.querySelector('#save');
+const inputReset = document.querySelector('#reset');
+
+/* REGEX */
+
+let regexInputStartDate = new RegExp('(0[1-9]|[1-3][0-9])\/(0[1-9]|1[0-2])\/[1-9][0-9]{3}');
+let regexInputWeek = new RegExp('[0-9]{1,2}');
+let regexInputWeeklyProducts = new RegExp('[0-9]{1,3}');
+let regexInputDaysInaWeek = new RegExp('[1-9]');
+let regexInputCheckTreshold = new RegExp('[1-9]');
+
+let inputs = [inputStartDate, inputWeeks, inputWeeklyProducts, inputDaysInaWeek, inputCheckTreshold];
+
+/* disable paste into inputs */
+
+inputs.forEach((input) => input.addEventListener('paste', (e) => e.preventDefault()));
+
+/* disable autocomplete inputs */
+
+inputs.forEach((input) => input.autocomplete = 'off');
+
+inputStartDate.addEventListener('input', (e) => validator.checkStartDate(inputStartDate, e, 10, regexInputStartDate, inputs, inputSave));
+inputWeeks.addEventListener('input', (e) => validator.checkOtherInputs(inputWeeks, e, 2, regexInputWeek, inputs, inputSave));
+inputWeeklyProducts.addEventListener('input', (e) => validator.checkOtherInputs(inputWeeklyProducts, e, 3, regexInputWeeklyProducts, inputs, inputSave));
+inputDaysInaWeek.addEventListener('input', (e) => validator.checkOtherInputs(inputDaysInaWeek, e, 1, regexInputDaysInaWeek, inputs, inputSave));
+inputCheckTreshold.addEventListener('input', (e) => validator.checkOtherInputs(inputCheckTreshold, e, 1, regexInputCheckTreshold, inputs, inputSave));
+
+//TODO: finire di gestire il save
+
+inputSave.addEventListener('click', (e) => {e.preventDefault(); console.log('tutto ok!')});
+inputReset.addEventListener('click', () => inputs.forEach(input => input.classList.remove('valid-input', 'error-input')));
+
  let init = () => {
 
 	let runtime = cnf.weeksRuntime;
