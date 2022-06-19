4f624022b3d0b7a66bd5596e549a79f4afb85293
Davide Murroni <davide.murroni@edu.itspiemonte.it>
2022-06-19 04:06:25 +0200

    fixed setting panel style / implemented test.js

--- scripts/test.js
+++ scripts/test.js
@@ -2,11 +2,14 @@
 
 let openCloseMenu = () => {
   let mainContainer = document.getElementById("main-container");
+  let buttonContainer = document.getElementById("form-button-container")
   let button = document.getElementById("setting-btn");
-  if (mainContainer.className == "container") {
+  if (mainContainer.className == "container" && buttonContainer.className == "container") {
     mainContainer.className += "-open";  
+    buttonContainer.className += "-open-btn";  
   } else {
     mainContainer.className = "container";  
+    buttonContainer.className = "container";  
   }
   if (button.className == "setting-btn") {
     button.className += "-open";
